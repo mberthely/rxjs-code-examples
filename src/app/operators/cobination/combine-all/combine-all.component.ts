4c90b7edbe487bf0734cb52a4c18cf9a2cf84ce7
mario-legion <berthely@gmail.com>
2023-09-03 01:02:23 -0600

    Update implementation

--- src/app/operators/cobination/combine-all/combine-all.component.ts
+++ src/app/operators/cobination/combine-all/combine-all.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { from, of } from 'rxjs';
+import { combineAll } from 'rxjs/operators';
 
 @Component({
   selector: 'app-combine-all',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CombineAllComponent implements OnInit {
 
+  data = of([1, 2, 3, 4])
+
   constructor() { }
 
   ngOnInit(): void {
+
+    // TODO: find a good example this does not work or not able to understand
+    this.data.pipe(
+      combineAll()
+    ).subscribe(result => { console.log(result) })
+
+    this.data.subscribe(result => { console.log('no combine ' + result) })
+
   }
 
 }
